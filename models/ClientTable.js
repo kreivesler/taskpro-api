562b12a3c58810f14cf3a05a252bcd84004f66a3
kreivesler <rileykreive@gmail.com>
2025-09-22 00:26:31 -0300

    new model user and update model client

--- models/ClientTable.js
+++ models/ClientTable.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 const bcrypt = require("bcrypt");
+const User = require("./UserTable");
 
 class Client extends Model {}
 
@@ -17,16 +18,16 @@ Client.init(
       allowNull: false,
     },
     email: {
-      type: DataTypes.STRING(255),
+      type: DataTypes.STRING(100),
       allowNull: false,
       unique: true,
     },
     document: {
-      type: DataTypes.STRING(255),
+      type: DataTypes.STRING(15),
       allowNull: false,
     },
     phone: {
-      type: DataTypes.STRING(45),
+      type: DataTypes.STRING(15),
     },
     password: {
       type: DataTypes.STRING(255),
@@ -41,19 +42,25 @@ Client.init(
       beforeCreate: async (client) => {
         const salt = await bcrypt.genSalt(10);
         client.password = await bcrypt.hash(client.password, salt);
-        client.email = client.email.toLowerCase();
+        client.name = client.name.toLowerCase().trim();
+        client.email = client.email.toLowerCase().trim();
       },
       beforeUpdate: async (client) => {
         if (client.changed("password")) {
           const salt = await bcrypt.genSalt(10);
           client.password = await bcrypt.hash(client.password, salt);
         }
+        if (client.changed("name")) {
+          client.name = client.name.toLowerCase().trim();
+        }
         if (client.changed("email")) {
-          client.email = client.email.toLowerCase();
+          client.email = client.email.toLowerCase().trim();
         }
       },
     },
   }
 );
+
+Client.hasMany(User, { foreignKey: "client_id" });
 
 module.exports = Client;
---
+++ models/UserTable.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const bcrypt = require("bcrypt");
+const Client = require("./ClientTable");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    client_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Client,
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    tableName: "user",
+    timestamps: false,
+    hooks: {
+      beforeCreate: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        user.name = user.name.toLowerCase().trim();
+        user.email = user.email.toLowerCase().trim();
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed("password")) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+        if (user.changed("name")) {
+          user.name = user.name.toLowerCase().trim();
+        }
+        if (user.changed("email")) {
+          user.email = user.email.toLowerCase().trim();
+        }
+      },
+    },
+  }
+);
+
+module.exports = User;
