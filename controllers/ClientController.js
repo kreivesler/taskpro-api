436290f7117f8b08045d9078307017cb23a06215
kreivesler <rileykreive@gmail.com>
2025-09-23 16:25:56 -0300

    refatored code and aply cache with redis

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -1,5 +1,6 @@
 const { decrypt } = require("../utils/CryptoDocument");
 const Client = require("../models/ClientTable");
+const { redisClient } = require("../redis/client");
 
 module.exports = clientController = {
   newClient: async (req, res) => {
@@ -11,7 +12,7 @@ module.exports = clientController = {
       });
 
       if (clientExists.length > 0) {
-        return res.status(404).json({
+        return res.status(400).json({
           message: "Client exist for this email. Try again with other email.",
         });
       }
@@ -22,6 +23,16 @@ module.exports = clientController = {
         return res.status(500).json({ message: "Error at create new client." });
       }
 
+      await redisClient.set(
+        `clientInfo${client.id}`,
+        JSON.stringify({
+          id: client.id,
+          name: client.name,
+          email: client.email,
+          document: decrypt(client.document),
+          phone: client.phone,
+        })
+      );
       return res.status(201).json({ message: "Client created successfully!" });
     } catch (error) {
       console.log("internal server error", error.message);
@@ -35,6 +46,20 @@ module.exports = clientController = {
         return res.status(404).json({ message: "Client not found." });
       }
       await client.update(req.body);
+      const clientStore = await redisClient.get(
+        `clientInfo${parseInt(req.params.id)}`
+      );
+
+      const clientObj = JSON.parse(clientStore);
+      clientObj.name = client.name;
+      clientObj.email = client.email;
+      clientObj.document = decrypt(client.document);
+      clientObj.phone = client.phone;
+
+      await redisClient.set(
+        `clientInfo${client.id}`,
+        JSON.stringify(clientObj)
+      );
 
       return res.status(200).json({ message: "Client updated successfully!" });
     } catch (error) {
@@ -44,19 +69,38 @@ module.exports = clientController = {
   },
   getClientById: async (req, res) => {
     try {
-      const client = await Client.findByPk(parseInt(req.params.id));
-      if (!client) {
-        return res
-          .status(404)
-          .json({ message: "Client not found or not exist." });
+      let client;
+
+      const clientStore = await redisClient.get(
+        `clientInfo${parseInt(req.params.id)}`
+      );
+
+      if (clientStore) {
+        client = JSON.parse(clientStore);
+      } else {
+        const clientDb = await Client.findByPk(parseInt(req.params.id));
+
+        if (!clientDb) {
+          return res
+            .status(404)
+            .json({ message: "Client not found or not exist." });
+        }
+
+        client = {
+          id: clientDb.id,
+          name: clientDb.name,
+          email: clientDb.email,
+          document: decrypt(clientDb.document),
+          phone: clientDb.phone,
+        };
+
+        await redisClient.set(
+          `clientInfo:${client.id}`,
+          JSON.stringify(client)
+        );
       }
-      return res.status(200).json({
-        id: client.id,
-        name: client.name,
-        email: client.email,
-        document: await decrypt(client.document),
-        phone: client.phone,
-      });
+
+      return res.status(200).json(client);
     } catch (error) {
       console.log("internal server error", error.message);
       return res.status(500).json("Internal server error.");
@@ -64,15 +108,32 @@ module.exports = clientController = {
   },
   getAllTask: async (req, res) => {
     try {
-      const client = await Client.findByPk(parseInt(req.params.id));
-      if (!client) {
-        return res
-          .status(404)
-          .json({ message: "Client not found or not exist." });
-      }
-      const allTasks = await client.getTasks();
+      let clientTasks;
 
-      return res.status(200).json(allTasks);
+      const taskStore = await redisClient.get(
+        `clientTasks${parseInt(req.params.id)}`
+      );
+
+      if (taskStore) {
+        clientTasks = JSON.parse(taskStore);
+      } else {
+        const clientDb = await Client.findByPk(parseInt(req.params.id));
+
+        if (!clientDb) {
+          return res
+            .status(404)
+            .json({ message: "Client not found or not exist." });
+        }
+
+        clientTasks = await clientDb.getTasks();
+
+        await redisClient.set(
+          `clientTasks${parseInt(clientDb.id)}`,
+          JSON.stringify(clientTasks)
+        );
+      }
+
+      return res.status(200).json(clientTasks);
     } catch (error) {
       console.log("internal server error", error.message);
       return res.status(500).json("Internal server error.");
@@ -80,20 +141,35 @@ module.exports = clientController = {
   },
   getUsersByClient: async (req, res) => {
     try {
-      const client = await Client.findByPk(parseInt(req.params.id));
-      if (!client) {
-        return res
-          .status(404)
-          .json({ message: "Client not found or not exist." });
-      }
+      let allUsers;
 
-      const users = await client.getUsers();
-      const allUsers = users.map((u) => {
-        return {
-          id: u.id,
-          name: u.name,
-        };
-      });
+      const clientUsers = await redisClient.get(
+        `clientUsers${parseInt(req.params.id)}`
+      );
+
+      if (clientUsers) {
+        allUsers = JSON.parse(clientUsers);
+      } else {
+        const clientDb = await Client.findByPk(parseInt(req.params.id));
+        if (!clientDb) {
+          return res
+            .status(404)
+            .json({ message: "Client not found or not exist." });
+        }
+
+        const users = await clientDb.getUsers();
+        allUsers = users.map((u) => {
+          return {
+            id: u.id,
+            name: u.name,
+          };
+        });
+
+        await redisClient.set(
+          `clientUsers${parseInt(clientDb.id)}`,
+          JSON.stringify(allUsers)
+        );
+      }
 
       return res.status(200).json(allUsers);
     } catch (error) {
