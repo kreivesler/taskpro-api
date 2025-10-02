418639cbde7e19601510ca7173095410922e7081
kreivesler <rileykreive@gmail.com>
2025-10-02 08:32:21 -0300

    correct import for SecurityMiddleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
-const redisSecurityMiddleware = require("./midlewares/SecurityMidleware");
+const redisSecurityMiddleware = require("./midlewares/SecurityMiddleware");
 const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 const sequelize = require("./config/db");
