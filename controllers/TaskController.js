d956538c04770d3995f90f666f0054d7b834179b
kreivesler <rileykreive@gmail.com>
2025-10-16 11:56:28 -0300

    new methods for taskController

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -20,4 +20,51 @@ module.exports = taskController = {
       return res.status(500).json({ message: "Internal server error." });
     }
   },
+  updateStatus: async (req, res) => {
+    try {
+      const id = req.body.taskId;
+      const dbTask = await Task.findByPk(id);
+      if (!dbTask) {
+        return res.status(404).json({ message: "Task not found." });
+      }
+
+      const newStatusForTask = req.body.status;
+
+      if (dbTask.status !== newStatusForTask) {
+        await dbTask.update({ status: newStatusForTask });
+      }
+
+      await redisClient.set(`task${dbTask.id}`, JSON.stringify(dbTask));
+
+      return res
+        .status(201)
+        .json({ message: "Task status updated successfully!" });
+    } catch (error) {
+      console.log("Internal server error:", error.message);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  },
+  getTaskById: async (req, res) => {
+    try {
+      const taskId = req.params.id;
+
+      const taskCashedString = await redisClient.get(`task${taskId}`);
+
+      let dataTask;
+      if (taskCashedString) {
+        dataTask = JSON.parse(taskCashedString);
+      } else {
+        const dbTask = await Task.findByPk(taskId);
+
+        await redisClient.set(`task${dbTask.id}`, dbTask);
+
+        dataTask = dbTask;
+      }
+
+      return res.status(200).json(dataTask);
+    } catch (error) {
+      console.log("Internal server error:", error.message);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+  },
 };
