1bb07fa2951b675dde4843df1ed8ed0cd9aebaa8
kreivesler <rileykreive@gmail.com>
2025-10-21 09:38:34 -0300

    new method for middleware

--- midlewares/TaskMiddleware.js
+++ midlewares/TaskMiddleware.js
@@ -1,5 +1,7 @@
 const Client = require("../models/ClientTable");
 const User = require("../models/UserTable");
+const { redisClient } = require("../redis/client");
+
 module.exports = taskMiddleware = {
   verifyTaskBeforeCreate: async (req, res, next) => {
     try {
@@ -26,6 +28,35 @@ module.exports = taskMiddleware = {
 
       if (![title, description, date].every(Boolean)) {
         return res.status(400).json({ message: "Task info not declared" });
+      }
+
+      next();
+    } catch (error) {
+      console.error("Internal server error", error.message);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  verifyTaskOnUpdateStatus: async (req, res, next) => {
+    try {
+      const id = req.body.id;
+      const newStatusTask = req.body.status;
+      const statusBase = ["pending", "confirmed", "canceled"];
+      const taskIdIsValid = isNaN(id);
+
+      if (!taskIdIsValid) {
+        return res.status(400).json({ message: "This ID is invalid." });
+      }
+
+      const taskExists = await redisClient.get(`task${id}`);
+
+      if (taskExists.length <= 0) {
+        return res.status(400).json({ message: "The task not found." });
+      }
+
+      if (!statusBase.includes(newStatusTask)) {
+        return res
+          .status(400)
+          .json({ message: "The status declared is incorrect." });
       }
 
       next();
